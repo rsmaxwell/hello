aff817e118abaf78e042a37736f81fd39ba97b7f
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-01-29 21:13:53 +0000

    update version header with timestamp

--- pre-build.bat
+++ pre-build.bat
@@ -7,16 +7,16 @@ set VERSION=0.0.%BUILD_ID%
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-set "TIMESTAMP=%YYYY%%MM%%DD%-%HH%%Min%%Sec%"
+set "BUILD_DATE=%YYYY%%MM%%DD%-%HH%%Min%%Sec%"
 
-echo timestamp: "%TIMESTAMP=%"
+echo BUILD_DATE: "%BUILD_DATE=%"
 
  
 FOR /R ".\src\" %%G in (*version.h) DO (
    Echo File: %%G
    powershell -Command "(gc %%G) -replace '<VERSION>',    '%VERSION%'    | Out-File -encoding ASCII %%G"
    powershell -Command "(gc %%G) -replace '<BUILD_ID>',   '%BUILD_ID%'   | Out-File -encoding ASCII %%G"
-   powershell -Command "(gc %%G) -replace '<BUILD_DATE>', '%TIMESTAMP%'  | Out-File -encoding ASCII %%G"
+   powershell -Command "(gc %%G) -replace '<BUILD_DATE>', '%BUILD_DATE%' | Out-File -encoding ASCII %%G"
    powershell -Command "(gc %%G) -replace '<GIT_COMMIT>', '%GIT_COMMIT%' | Out-File -encoding ASCII %%G"
    powershell -Command "(gc %%G) -replace '<GIT_BRANCH>', '%GIT_BRANCH%' | Out-File -encoding ASCII %%G"
    powershell -Command "(gc %%G) -replace '<GIT_URL>',    '%GIT_URL%'    | Out-File -encoding ASCII %%G"
